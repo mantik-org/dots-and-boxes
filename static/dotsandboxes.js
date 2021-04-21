02ab81784edb160d38ef53db98698c3c664e1a2d
Antonino Natale <antonio.natale97@hotmail.com>
2021-04-21 17:38:39 +0200

    Update dotsandboxes.js

--- static/dotsandboxes.js
+++ static/dotsandboxes.js
@@ -404,6 +404,7 @@
       var address = document.getElementById('agent'+i).value;
       console.log('Address agent'+i+': '+address);
       if (address) {
+        address = window.location.origin.replace(/^http/, 'ws') + ':' + address;
         console.log("Starting websocket for agent "+i+" on address "+address);
         var agent = agents[i];
         agent.address = address;
